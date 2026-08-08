d994e0d4ef3da3a90febee051db1cda039f0b0da
namnkahn1607 <nlnkandthebois@gmail.com>
2026-08-08 23:37:15 +0700

    fix: missing ctor of singly linked list

--- include/dsa/singly_linked_list.h
+++ include/dsa/singly_linked_list.h
@@ -6,6 +6,10 @@ struct ListNode {
     int       val;
     ListNode* next;
 
+    ListNode()
+        : val(0)
+        , next(nullptr) {}
+
     ListNode(int x)
         : val(x)
         , next(nullptr) {}
