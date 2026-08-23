970d10d044393c6151a3f2a9392e1add93ca622e
namnkahn1607 <nlnkandthebois@gmail.com>
2026-08-23 10:19:31 +0700

    feat: 0133 Clone Graph - Iterative DFS

---
+++ src/0133-clone-graph/133b.cpp
@@ -0,0 +1,74 @@
+// 133b. Clone Graph
+
+#include <stack>
+#include <unordered_map>
+
+#include "ampio/stdin.h"
+#include "ampio/stdout.h"
+#include "graph.h"
+
+struct Frame {
+    Node* cloned_parent;
+    Node* original_child;
+};
+
+Node* cloneGraph(Node* node) {
+    if (node == nullptr) {
+        return nullptr;
+    }
+
+    std::stack<Frame> callstack;
+    callstack.push({nullptr, node});
+
+    std::unordered_map<int, Node*> table;
+
+    while (!callstack.empty()) {
+        auto [cloned_parent, original_child] = callstack.top();
+        callstack.pop();
+
+        int val = original_child->val;
+        if (table.count(val)) {
+            if (cloned_parent != nullptr) {
+                cloned_parent->neighbors.push_back(table[val]);
+            }
+            continue;
+        }
+
+        Node* cloned_child = new Node(val);
+        if (cloned_parent != nullptr) {
+            cloned_parent->neighbors.push_back(cloned_child);
+        }
+        table[val] = cloned_child;
+
+        for (auto* neighbor : original_child->neighbors) {
+            callstack.push({cloned_child, neighbor});
+        }
+    }
+
+    return table[node->val];
+}
+
+int main() {
+    // `0 <= The number of nodes in the graph <= 100`.
+    // `1 <= Node.val <= 100`.
+    // There are no duplicate edges and no self-loops in the graph.
+    auto adj_list = ampio::ReadMatrix<int>();
+
+    const size_t       kNumNodes = adj_list.size();
+    std::vector<Node*> nodes(kNumNodes + 1);
+    for (size_t i = 1; i <= kNumNodes; ++i) {
+        nodes[i] = new Node(i);
+    }
+
+    for (size_t i = 0; i < kNumNodes; ++i) {
+        int   curr      = i + 1;
+        Node* curr_node = nodes[curr];
+        for (auto& next : adj_list[i]) {
+            curr_node->neighbors.push_back(nodes[next]);
+        }
+    }
+
+    Node* original = nodes[1];
+    ampio::Print(Node::Compare(original, cloneGraph(original)));
+    return 0;
+}
